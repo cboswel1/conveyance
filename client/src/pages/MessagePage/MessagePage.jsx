1ccf7e2ac115b32d5ae272ff515c72badf5bb4f9
cboswel1 <christianboswell86@gmail.com>
2021-01-06 21:05:57 -0700

    created message page

---
+++ client/src/pages/MessagePage/MessagePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+const MessagePage = () => {
+
+    return (
+        <div>
+          Hello from Message page
+        </div>
+      );
+    };
+    
+    export default MessagePage;
