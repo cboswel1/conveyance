34b1fdeadfaa17986c8a11f108ff24c04c9c741f
Vivid Green <zacharycreek@gmail.com>
2021-01-09 14:51:38 -0700

    Added cors middleware back in to server.js

--- server/server.js
+++ server/server.js
@@ -9,12 +9,11 @@ const routes = require('./routes');
 const PORT = process.env.PORT || 5000;
 const db = require("./models");
 
-// var corsOptions = {
-//   origin: "http://localhost:5001"
-// };
+var corsOptions = {
+  origin: "http://localhost:5001"
+};
 
-// app.use(cors(corsOptions));
-// app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(routes);
