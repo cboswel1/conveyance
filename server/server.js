467694a825212389890d48da85d8502899187eeb
Vivid Green <zacharycreek@gmail.com>
2021-01-09 14:36:02 -0700

    removed cors

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const db = require("./models");
 // };
 
 // app.use(cors(corsOptions));
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(routes);
